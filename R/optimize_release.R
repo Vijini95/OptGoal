7351b0a1ca2ea74b843ad62820c0aebad55168b4
Vijini <Vijinil@tamu.edu>
2024-11-09 22:18:38 -0600

    Include the description of parameters

--- R/optimize_release.R
+++ R/optimize_release.R
@@ -1,4 +1,8 @@
-optimize_release<-function(){
-
-
-}
+#' Optimize Water Release Using Goal Programming
+#'
+#' This function optimizes water release strategies using goal programming.
+#'
+#' @param inflow Numeric vector of inflow amounts.
+#' @param demand Numeric vector of demand amounts.
+#' @param constraints List of constraints for the optimization.
+#' @param goals List of goals for the optimization.
