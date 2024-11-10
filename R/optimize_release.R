fa6173f89719cfdf198fdafde1f418c1e1e26236
Vijini <Vijinil@tamu.edu>
2024-11-10 13:22:54 -0600

    Add an example

--- R/optimize_release.R
+++ R/optimize_release.R
@@ -16,7 +16,16 @@
 #' @examples
 #' \dontrun{
 #' # Example usage
-#' optimized_release <- optimize_release(inflow, demand, constraints, goals)
+#' inflow <- c(100, 120, 90, 110, 95)
+#' demand <- c(80, 85, 75, 90, 80)
+#' S_min_vector <- c(200, 220, 240, 260, 280)
+#' S_max_vector <- c(1000, 980, 960, 940, 920)
+#' constraints <- list(
+#'   initial_storage = 500,
+#'   S_min = S_min_vector,
+#'   S_max = S_max_vector
+#' )
+#' result <- optimize_release(inflow, demand, constraints)
 #' }
 #' @export
 optimize_release <- function(inflow, demand, constraints, goals) {
