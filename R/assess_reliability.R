4dfb1451e3fa40ad47e2871af2bc17d94af779f3
Vijini <Vijinil@tamu.edu>
2024-11-21 01:19:15 -0600

    Add previous code as example

--- R/assess_reliability.R
+++ R/assess_reliability.R
@@ -8,9 +8,20 @@
 #'
 #' @return A data frame containing the reliability metrics.
 #' @examples
-#' release <- result$release
+#' # Example usage
+#' inflow <- c(7569, 6383, 8472, 13084, 9689,
+#' 4301, 399, 2896, 2723, 7239, 14849, 13304)
 #' demand <- c(1586.72, 1057.81, 1586.72, 1057.81, 1586.72,
 #' 1057.81, 1586.72, 1057.81, 1586.72, 1057.81, 1586.72, 1057.81)
+#' S_min_vector <- rep(2824.55, time=12)
+#' S_max_vector <- rep(26864.25, time=12)
+#' constraints <- list(
+#'   initial_storage = 500,
+#'   S_min = S_min_vector,
+#'   S_max = S_max_vector
+#' )
+#' result <- optimize_release(inflow, demand, constraints)
+#' release <- result$release
 #' reliability_results <- assess_reliability(release, demand)
 #' print(reliability_results)
 #' @export
